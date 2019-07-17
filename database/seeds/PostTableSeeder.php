f4dcc23be99b5838a4f828fe9ef841f61d7ab144
veropra <prainoveronica@gmail.com>
2019-07-18 00:03:39 +0200

    arrivata al punto faker

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Fake\Factory as Faker;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
