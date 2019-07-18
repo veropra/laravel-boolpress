fe7964dbc373ee5cac1144902943334a6749d78a
veropra <prainoveronica@gmail.com>
2019-07-18 18:39:55 +0200

    faker ok

--- database/migrations/2019_07_17_212624_create_posts_table.php
+++ database/migrations/2019_07_17_212624_create_posts_table.php
@@ -6,27 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
+            $table->string('title')->unique();
             $table->longText('content');
             $table->string('author');
+            $table->string('slug');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('posts');
--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -1,17 +1,33 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Fake\Factory as Faker;
+use Faker\Factory as Faker;
+use App\Post;
+use Illuminate\Support\Str;
 
 class PostTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+      $faker = Faker::create();
+
+      for ($i=0; $i < 20; $i++) {
+          $post = new Post();
+          /*
+          $post->title = $faker->sentence();
+          $post->content = $faker->text(2000);
+          $post->author = $faker->firstName . ' ' . $faker->lastName;
+          $post->slug = Str::slug($post->title);
+          */
+          $titolo = $faker->sentence();
+          $dati_post = [
+            'title' => $titolo,
+            'content' => $faker->text(2000),
+            'author' => $faker->firstName . ' ' . $faker->lastName,
+            'slug' => Str::slug($titolo)
+          ];
+          $post->fill($dati_post);
+          $post->save();
+      }
     }
 }
